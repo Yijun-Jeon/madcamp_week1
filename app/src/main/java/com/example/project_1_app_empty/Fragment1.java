3d076efc4eddcbf1b38cc195c336ee7d76f9cef0
Yijun Jeon <gkqls813@gmail.com>
2022-07-02 13:26:23 +0900

    Make tab1 RecyclerView & Plus Button

--- app/src/main/java/com/example/project_1_app_empty/Fragment1.java
+++ app/src/main/java/com/example/project_1_app_empty/Fragment1.java
@@ -9,12 +9,26 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Fragment1 extends Fragment {
+
+    private ArrayList<PhoneBook> mPhonebook;
+
+    public Fragment1(){
+        super();
+        mPhonebook = new ArrayList<PhoneBook>();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Button button;
         View view;
+        RecyclerView recyclerView;
+        RecyclerAdapter recyclerAdapter;
 
         view = inflater.inflate(R.layout.fragment1, container, false);
         button = view.findViewById(R.id.button_f1);
@@ -22,8 +36,19 @@ public class Fragment1 extends Fragment {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getContext(), "done", Toast.LENGTH_LONG).show();
+                //Intent intent = new Intent(getActivity(),Addphone)
             }
         });
+
+        recyclerView = (RecyclerView)view.findViewById(R.id.recyclerView);
+        recyclerAdapter = new RecyclerAdapter();
+        recyclerView.setAdapter(recyclerAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        for(int i=1;i<=10;i++){
+            mPhonebook.add(new PhoneBook(i+"번째 사람","000-0000-000"+i));
+        }
+        recyclerAdapter.setMyPhoneBook(mPhonebook);
 
         return view;
     }
---
+++ app/src/main/java/com/example/project_1_app_empty/RecyclerAdapter.java
@@ -0,0 +1,55 @@
+package com.example.project_1_app_empty;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private ArrayList<PhoneBook> myPhoneBook;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycleview,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.onBind(myPhoneBook.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return myPhoneBook.size();
+    }
+
+    public void setMyPhoneBook(ArrayList<PhoneBook> list){
+        myPhoneBook = list;
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        TextView phone;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.name);
+            phone = (TextView) itemView.findViewById(R.id.phone);
+        }
+
+        void onBind(PhoneBook item){
+            name.setText(item.getName());
+            phone.setText(item.getPhone());
+        }
+    }
+}
