f9213192ed73fd77dee32a4d27d855ea43e632df
antaechan <0627antaechan@gmail.com>
2022-07-03 20:35:59 +0900

    trash

--- app/src/main/java/com/example/project_1_app_empty/Fragment3.java
+++ app/src/main/java/com/example/project_1_app_empty/Fragment3.java
@@ -1,15 +1,74 @@
 package com.example.project_1_app_empty;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 public class Fragment3 extends Fragment {
+
+    TextView textView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment3, container, false);
+        View view;
+        Button button;
+
+        view = inflater.inflate(R.layout.fragment3, container, false);
+
+        textView = view.findViewById(R.id.textView);
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLocationService(getActivity().getApplicationContext());
+            }
+        });
+
+        return view;
+    }
+
+    class GPSListener implements LocationListener {
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            String message = "최근 위치->Latitude: " + latitude + "\nLongitude:" + longitude;
+
+            textView.setText(message);
+
+        }
+
+        public void onProviderDisabled(String provider) { }
+
+        public void onProviderEnabled(String provider) { }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+    }
+
+    public void startLocationService(Context context) {
+        LocationManager manager =  (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            GPSListener gpsListener = new GPSListener();
+            long minTime = 10000;
+            float minDistance = 0;
+
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, gpsListener);
+            Toast.makeText(getActivity().getApplicationContext(), "내 위치 확인 요청함", Toast.LENGTH_SHORT).show();
+
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
     }
 }
